8cd7d29e5671d59833e1ad07d4a3561e5632f5fb
taoyuanxiaoqi <15891428860@163.com>
2016-04-12 16:52:09 +0800

    first commit，add simples and test lib

---
+++ lib/src/main/java/com/taoyuanxiaoqi/lib/Utils.java
@@ -0,0 +1,10 @@
+package com.taoyuanxiaoqi.lib;
+
+/**
+ * Created by lee on 16/4/12.
+ */
+public class Utils {
+    public static String getVersison(){
+        return "current version is 1.0 hhhhhhh";
+    }
+}
