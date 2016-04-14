357b692a1eb3281c30f9cc60f34b7bb457b2d9e2
taoyuanxiaoqi <15891428860@163.com>
2016-04-14 14:59:04 +0800

    引入子目录库内容

--- app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
+++ app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.taoyuanxiaoqi.lib.Utils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView)findViewById(R.id.tv_app);
-//        tv.setText(Utils.get);
+        tv.setText("App Projects " + Utils.getVersison());
     }
 }
