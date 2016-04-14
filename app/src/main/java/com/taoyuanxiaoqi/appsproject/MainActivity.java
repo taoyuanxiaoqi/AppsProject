f53622aab5b11f435ebb8c3f4697ddd9308798cc
taoyuanxiaoqi <15891428860@163.com>
2016-04-14 17:40:11 +0800

    加入第二个子模块

--- app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
+++ app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.taoyuanxiaoqi.color.ColorUtil;
 import com.taoyuanxiaoqi.lib.Utils;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView)findViewById(R.id.tv_app);
+        tv.setTextColor(ColorUtil.getConfigColor());
         tv.setText("App Projects " + Utils.getVersison());
     }
 }
