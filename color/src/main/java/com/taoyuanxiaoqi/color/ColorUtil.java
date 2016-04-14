d8afc174c632a16adef8b3d263f49e7599c5b566
taoyuanxiaoqi <15891428860@163.com>
2016-04-14 17:30:39 +0800

    first commit

---
+++ color/src/main/java/com/taoyuanxiaoqi/color/ColorUtil.java
@@ -0,0 +1,14 @@
+package com.taoyuanxiaoqi.color;
+
+import android.graphics.Color;
+import android.util.Log;
+
+/**
+ * Created by lee on 16/4/14.
+ */
+public class ColorUtil {
+    public static int getConfigColor(){
+        Log.d("hehe","first..");
+        return Color.GREEN;
+    }
+}
