739fd4a5df65bae2d6ac98134e4c1289cf987fc3
taoyuanxiaoqi <15891428860@163.com>
2016-04-14 19:41:57 +0800

    reset

--- app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
+++ app/src/main/java/com/taoyuanxiaoqi/appsproject/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.taoyuanxiaoqi.color.ColorUtil;
 import com.taoyuanxiaoqi.lib.Utils;
+
+//import com.taoyuanxiaoqi.color.ColorUtil;
+//import com.taoyuanxiaoqi.lib.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView)findViewById(R.id.tv_app);
-        tv.setTextColor(ColorUtil.getConfigColor());
+//        tv.setTextColor(ColorUtil.getConfigColor());
         tv.setText("App Projects " + Utils.getVersison());
     }
 }
